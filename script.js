4d6150ca05009c31af1d195f2cfe0512e323557b
mukhr <mukhir98@gmail.com>
2022-07-18 18:53:29 +0500

    javastepen

---
+++ script.js
@@ -0,0 +1,49 @@
+let a = +prompt('Введите число');
+let b = +prompt('Введите степень');
+let num = 1
+if(!isNaN(a) && !isNaN (b) && a != 0 && b != 0 ){
+  for(let i = 1; i <= b; i++ ) {
+      num = num * a
+  }
+  alert(num);
+}else if (!isNaN(a) && !isNaN (b) && a != 0 && b == 0 ){
+    
+  
+  for(let i = 1; i <= (b = 2); i++ ) {
+      num = num * a
+  }
+  alert(num);
+} else if (!isNaN(a) && !isNaN(b) && a == 0 && b != 0){
+
+  let a = +prompt('Введите число не равное ( 0 )');
+  let b = +prompt('Введите степень в которую нужно возвести число');
+
+
+  for(let i = 1; i <= (b = 2); i++ ) {
+      num = num * a
+  }
+  alert(num);
+    
+} else if (!isNaN(a) && !isNaN(b) && a == 0 && b == 0){
+  let a = +prompt('Введите число не равное ( 0 )');
+  let b = +prompt('Введите степень в которую нужно возвести число');
+
+
+  for(let i = 1; i <= (b = 2); i++ ) {
+      num = num * a
+  }
+  alert(num);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
